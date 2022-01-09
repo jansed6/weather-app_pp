f456215ffd937dee9602c7214fa308977baab460
Eduard Janský <Eduard.Jansky@tescosw.cz>
2022-01-09 18:07:03 +0100

    opravy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import {Routes, Route, Navigate} from 'react-router-dom';
-import Search from './Pages/Search';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Search from "./Pages/Search";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="*"  element={<Navigate to="/search" />} /> 
-        <Route path="/search" element={<Search />}  />
-      </Routes>    
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Navigate to="/search" />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
